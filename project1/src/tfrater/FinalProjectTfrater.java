97d0a6ad5b73404efaee0e11992df12c5aecdbdd
theod <theod@DESKTOP-4DJBL54.lan>
2023-12-13 12:29:52 -0500

    Updating my final project so it's saved

--- project1/src/tfrater/FinalProjectTfrater.java
+++ project1/src/tfrater/FinalProjectTfrater.java
@@ -9,33 +9,78 @@ import java.awt.event.WindowEvent;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.ArrayList;
+import java.util.LinkedList;
 import java.util.List;
 
 import javax.swing.JButton;
+import javax.swing.JComboBox;
+import javax.swing.JDialog;
 import javax.swing.JFileChooser;
 import javax.swing.JFrame;
+import javax.swing.JLabel;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.JScrollPane;
+import javax.swing.JTable;
 import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.table.DefaultTableModel;
 
 
 public class FinalProjectTfrater extends JFrame {
 	private static final long serialVersionUID = 3794059922116115530L;
 	//Text area where results and instructions are appended
 	private JTextArea outputArea = new JTextArea(20,20);
+	private String[][] itmList = {};
+	private String[] colNames = {"Type","Name","Amount","DueDate"};
+	private DefaultTableModel model = new DefaultTableModel(itmList, colNames);
+	private JTable outputTable = new JTable(model);
+	private JTextField typeField = new JTextField(10);
+	private JTextField nameField = new JTextField(10);
+	private JTextField amountField = new JTextField(10);
+	private JTextField dueDate = new JTextField(10);
+	
+	private class DataDialog extends JDialog {
+		private static final long serialVersionUID = 3794059922116115530L;
+		public DataDialog(JFrame parent) {
+			super(parent, "Data Editor",true);
+			JButton addButton = new JButton("Add");
+			JButton cancelButton = new JButton("Cancel");
+			JPanel buttonPanel = new JPanel(new GridLayout(1,2));
+			String[] comboChoices = {"Income","Expense"};
+			JComboBox<String> typeBox = new JComboBox<>(comboChoices);
+			buttonPanel.add(addButton);
+			buttonPanel.add(cancelButton);
+			JPanel quesPanel = new JPanel(new GridLayout(4,4,10,4));
+			quesPanel.add(new JLabel("Is this Income or an Expense?"));
+			quesPanel.add(typeBox);
+			getContentPane().setLayout(new BorderLayout());
+			getContentPane().add(quesPanel,BorderLayout.CENTER);
+			getContentPane().add(buttonPanel,BorderLayout.SOUTH);
+			setDefaultCloseOperation(JDialog.DISPOSE_ON_CLOSE);
+			setSize(500,200);
+		}
+	}
 	
 	public FinalProjectTfrater() {
 		super("Budget Calculator");
-		JButton submitButton = new JButton("Run");
+		JButton submitButton = new JButton("Open Budget");
+		JButton editButton = new JButton("Edit Budget");
 		JButton cancelButton = new JButton("Cancel");
-		JPanel buttonPanel = new JPanel(new GridLayout(1,2));
+		JPanel buttonPanel = new JPanel(new GridLayout(1,3));
 		buttonPanel.add(submitButton);
+		buttonPanel.add(editButton);
 		buttonPanel.add(cancelButton);
 		setLocationRelativeTo(null);
 		setSize(650,300);
 		getContentPane().setLayout(new BorderLayout());
 		getContentPane().add(new JScrollPane(outputArea),BorderLayout.CENTER);
+		getContentPane().add(new JScrollPane(outputTable),BorderLayout.CENTER);
 		outputArea.setEditable(false);
 		getContentPane().add(buttonPanel,BorderLayout.SOUTH);
 		submitButton.addActionListener(new ActionListener() {
@@ -43,6 +88,7 @@ public class FinalProjectTfrater extends JFrame {
 				try {
 					int incomeTotal = 0;
 					int expenseTotal = 0;
+					// https://docs.oracle.com/javase/tutorial/uiswing/components/filechooser.html
 					JFileChooser fileChooser = new JFileChooser();
 					int returnVal = fileChooser.showOpenDialog(FinalProjectTfrater.this);
 					if (returnVal == JFileChooser.APPROVE_OPTION) {
@@ -56,15 +102,16 @@ public class FinalProjectTfrater extends JFrame {
 							String name = splitList[1];
 							String amountString = splitList[2];
 							String dueDate = splitList[3];
-							String endDate = splitList[4];
-							if(type.equals("income")) {
-								int amount = Integer.parseInt(amountString);
-								incomeTotal += amount;
-							}
-							else if(type.equals("expense")) {
-								int amount = Integer.parseInt(amountString);
-								expenseTotal += amount;
-							}
+							String[] newLine = {type,name,amountString,dueDate};
+//							if(type.equals("income")) {
+//								int amount = Integer.parseInt(amountString);
+//								incomeTotal += amount;
+//							}
+//							else if(type.equals("expense")) {
+//								int amount = Integer.parseInt(amountString);
+//								expenseTotal += amount;
+//							}
+							model.addRow(newLine);
 						}
 						reader.close();
 					}
@@ -72,6 +119,17 @@ public class FinalProjectTfrater extends JFrame {
 					outputArea.append("You have "+spendable+" to spend this month");
 				}
 				catch (Exception excep) {
+					JOptionPane.showMessageDialog(null, "Please enter a number", "Incorrect Data Type", JOptionPane.ERROR_MESSAGE);
+				}
+			}
+		});
+		editButton.addActionListener(new ActionListener () {
+			public void actionPerformed(ActionEvent e) {
+				try {
+					DataDialog dlg = new DataDialog(null);
+					dlg.setVisible(true);
+				}
+				catch (Exception excep){
 					JOptionPane.showMessageDialog(null, "Please enter a number", "Incorrect Data Type", JOptionPane.ERROR_MESSAGE);
 				}
 			}
